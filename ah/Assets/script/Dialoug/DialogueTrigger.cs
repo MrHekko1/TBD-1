3f49ee30a56666c011d366c7322a2f0e7987be4b
Hekko <mrhekkoboi@gmail.com>
2021-08-10 16:51:49 +0200

    a lot of change
    a lot

---
+++ ah/Assets/script/Dialoug/DialogueTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+	public Dialogue dialogue;
+    public GameObject canvas;
+
+	public void TriggerDialogue ()
+	{
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            canvas.SetActive(true);
+	}
+       // void OnCollisionEnter2D(Collision2D col)
+       // {
+            
+       // }
+	public void Update()
+    {
+        
+       void OnCollisionEnter2D(Collision2D col)
+       {
+           TriggerDialogue();
+       }
+        
+    }
+      
+}
